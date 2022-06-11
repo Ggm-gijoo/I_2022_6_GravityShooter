f4d21b90158bb60bebd9fad4693e900c8dcf8988
Ggm-gijoo <gijoo1012@gmail.com>
2022-06-11 16:11:58 +0900

    2022/06/11
    플레이어 이동, 회전 구현
    애니메이션 구현

--- Assets/01_Scripts/PlayerMove.cs
+++ Assets/01_Scripts/PlayerMove.cs
@@ -5,23 +5,50 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private float moveSpeed = 10f;
+    private float rotateSpeed = 80f;
+    private float jumpPower = 5f;
+
     private Rigidbody playerRigid;
-    private float movevSpeed = 10f;
+    private Animator playerAnim;
 
     private void Start()
     {
         playerRigid = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+
+        playerAnim.Play("Idle");
     }
     private void Update()
     {
+        Move();
+        Jump();
     }
 
     public void Move()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        playerRigid.velocity = (h * Vector3.right + v * Vector3.forward) * movevSpeed;
+        float m = Input.GetAxis("Mouse X");
+
+        transform.Translate((Vector3.forward * v + Vector3.right * h) * Time.deltaTime * moveSpeed);
+        transform.Rotate(Vector3.up * m * rotateSpeed * Time.deltaTime);
+
+
+        playerAnim.SetFloat("Horizontal", h);
+        playerAnim.SetFloat("Vertical", v);
+
     }
+
+    public void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            playerAnim.SetTrigger("Jump");
+            playerRigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
 
 
 }
