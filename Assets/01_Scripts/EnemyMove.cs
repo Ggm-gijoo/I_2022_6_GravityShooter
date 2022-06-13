991d3f8c830038719e20a2aa4dd237789622346d
Ggm-gijoo <gijoo1012@gmail.com>
2022-06-13 10:59:24 +0900

    2022/06/13
    적이 블랙홀에 닿을 시 지속적으로 대미지를 입음

--- Assets/01_Scripts/EnemyMove.cs
+++ Assets/01_Scripts/EnemyMove.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    private float hp = 50f;
+    private bool isInhaled = false;
+
     private Animator enemyAnim;
 
     private void Start()
     {
         enemyAnim = GetComponent<Animator>();
         enemyAnim.SetBool("Open_Anim", true);
+    }
+
+    private IEnumerator enemyInhaled()
+    {
+        while(hp > 0 && isInhaled)
+        {
+            yield return new WaitForSeconds(0.5f);
+            hp -= 10f;
+            Debug.Log(hp);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BlackHole" && !isInhaled)
+        {
+            isInhaled = true;
+            StartCoroutine(enemyInhaled());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "BlackHole" && isInhaled)
+        {
+            isInhaled = false;
+            StopCoroutine(enemyInhaled());
+        }
     }
 }
